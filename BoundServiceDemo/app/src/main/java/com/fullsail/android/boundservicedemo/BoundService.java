86065ca35b8d1962a2c97f802a88aeec13bcd8d5
administrator <administrator@administrators-MacBook-Pro.local>
2014-09-05 09:26:22 -0400

    Week 1 - #3 - Service Bind + Unbind

---
+++ BoundServiceDemo/app/src/main/java/com/fullsail/android/boundservicedemo/BoundService.java
@@ -0,0 +1,53 @@
+package com.fullsail.android.boundservicedemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class BoundService extends Service {
+	
+	public class BoundServiceBinder extends Binder {
+		public BoundService getService() {
+			return BoundService.this;
+		}
+	}
+	
+	BoundServiceBinder mBinder;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mBinder = new BoundServiceBinder();
+		Toast.makeText(this, "Service Created", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+		return super.onStartCommand(intent, flags, startId);
+	}
+	
+	@Override
+	public void onDestroy() {
+		Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show();
+		super.onDestroy();
+	}
+	
+	@Override
+	public boolean onUnbind(Intent intent) {
+		Toast.makeText(this, "Service Unbound", Toast.LENGTH_SHORT).show();
+		return super.onUnbind(intent);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Toast.makeText(this, "Service Bound", Toast.LENGTH_SHORT).show();
+		return mBinder;
+	}
+	
+	public void showText(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+}
--- Fundamentals/app/src/main/java/com/fullsail/fundamentals/MainActivity.java
+++ Fundamentals/app/src/main/java/com/fullsail/fundamentals/MainActivity.java
@@ -1,87 +1,67 @@
 package com.fullsail.fundamentals;
 import android.app.Activity;
-import android.app.NotificationManager;
+import android.content.ComponentName;
 import android.content.Context;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.View.OnClickListener;
 import java.io.IOException;
 
+import com.fullsail.fundamentals.SimpleService.BoundServiceBinder;
 
-public class MainActivity extends Activity {
 
-    public static final int STANDARD_NOTIFICATION = 0x01001;
-    public static final int EXPANDED_NOTIFICATION = 0x01002;
+public class MainActivity extends Activity implements OnClickListener, ServiceConnection {
+
+    SimpleService mService;
+    boolean mBound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String uri1 = "android.resource://" + getPackageName() + "/raw/";
+        String uri = "android.resource://" + getPackageName() + "/" + R.raw.audio1;
+        findViewById(R.id.playButton).setOnClickListener(this);
+        findViewById(R.id.stopButton).setOnClickListener(this);
+        findViewById(R.id.nextButton).setOnClickListener(this);
 
-        String uri2 = "android.resource://" + getPackageName() + "/" + R.raw.audio1;
+    }
 
-// Player created, in idle state.
-        MediaPlayer player = new MediaPlayer();
-
-// Player initialized, in initialized state.
-        try {
-            player.setDataSource(this, Uri.parse(uri2));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-// Setting a prepared listener.
-        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                // Player prepared!
-                mp.start();
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SimpleService.class);
+        if(view.getId() == R.id.playButton) {
+            bindService(intent, this, Context.BIND_AUTO_CREATE);
+        } else if(view.getId() == R.id.stopButton) {
+            if(mBound) {
+                unbindService(this);
+                mBound = false;
+                mService = null;
             }
-        });
-
-        NotificationManager mgr =
-                (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setSmallIcon(R.drawable.ic_notification);
-        builder.setLargeIcon(BitmapFactory.decodeResource(
-                getResources(), R.drawable.ic_launcher));
-        builder.setContentTitle("Standard Title");
-        builder.setContentText("Standard Message");
-        NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
-        bigStyle.bigText("Some really long text goes here.");
-        bigStyle.setBigContentTitle("Expanded Title");
-        bigStyle.setSummaryText("Expanded Summary");
-        builder.setStyle(bigStyle);
-        mgr.notify(EXPANDED_NOTIFICATION, builder.build());
-
-    }
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        } else if (view.getId() == R.id.nextButton) {
+            if(mService != null) {
+                mService.showText("Next Pressed");
+            }
         }
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        BoundServiceBinder binder = (BoundServiceBinder)service;
+        mService = binder.getService();
+        mBound = true;
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        mService = null;
+        mBound = false;
+    }
+
 }
