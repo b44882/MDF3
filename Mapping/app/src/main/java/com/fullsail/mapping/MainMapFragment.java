07580bc9f9e6ce67b9e634e2e1ddc0635e8f8fcb
administrator <administrator@administrators-MacBook-Pro.local>
2014-09-23 22:31:40 -0400

    Week 4 #4 - Gets GPS Data

--- Mapping/app/src/main/java/com/fullsail/mapping/MainMapFragment.java
+++ Mapping/app/src/main/java/com/fullsail/mapping/MainMapFragment.java
@@ -1,8 +1,15 @@
 package com.fullsail.mapping;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,13 +23,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * Created by administrator on 9/23/14.
  */
-public class MainMapFragment extends MapFragment implements GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapClickListener {
+public class MainMapFragment extends MapFragment implements GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapClickListener, LocationListener {
 
     GoogleMap mMap;
+    LocationManager mManager;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        mManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+        Location loc = mManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double latitude = loc.getLatitude();
+        double longitude = loc.getLongitude();
 
         mMap = getMap();
         mMap.addMarker(new MarkerOptions().position(new LatLng(28.590647,-81.304510)).title("MDVBS Faculty Offices"));
@@ -33,8 +47,16 @@ public class MainMapFragment extends MapFragment implements GoogleMap.OnInfoWind
         mMap.setInfoWindowAdapter(new MarkerAdapter());
         mMap.setOnInfoWindowClickListener(this);
         mMap.setOnMapClickListener(this);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28.593770, -81.303797), 16));
+        if (loc != null){
+            mMap.addMarker(new MarkerOptions().position(new LatLng(loc.getLatitude(),loc.getLongitude())).title("GPS Home"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(loc.getLatitude(), loc.getLongitude()), 16));
+
+
+        } else {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28.593770, -81.303797), 16));
+        }
     }
+
 
     @Override
     public void onInfoWindowClick(final Marker marker) {
@@ -69,6 +91,27 @@ public class MainMapFragment extends MapFragment implements GoogleMap.OnInfoWind
                     }
                 })
                 .show();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("MainMapFragment: ", "Location changed to " + location);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 
